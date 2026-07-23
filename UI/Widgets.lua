cd6609eb7d986eb072060d9b4c1c07b836590840
fanguocai <guoc.fan@hotmail.com>
2026-07-23 21:41:31 +0800

    Add map portal icon background

--- UI/Widgets.lua
+++ UI/Widgets.lua
@@ -141,6 +141,7 @@ function Widgets:SetLocationEntry(button, entry, displayText, showIcon)
         atlas = catInfo and catInfo.atlas or Config.art.fallbackLocationAtlas
     end
     button.icon:SetAtlas(atlas, false)
+    button.iconBackground:SetShown(button.showIcon and entry.isMapPortal == true)
     self:UpdateLocationGeometry(button)
     button:Show()
 end
@@ -179,6 +180,10 @@ function Widgets:CreateLocationButton(parent, callbacks)
     button.highlight:Hide()
 
     button.iconBox = CreateFrame("Frame", nil, button)
+    button.iconBackground = button.iconBox:CreateTexture(nil, "BACKGROUND")
+    button.iconBackground:SetAllPoints(button.iconBox)
+    button.iconBackground:SetColorTexture(unpack(Config.colors.mapPortalIconBackground))
+    button.iconBackground:Hide()
     button.icon = button.iconBox:CreateTexture(nil, "ARTWORK")
     button.iconFrame = button.iconBox:CreateTexture(nil, "OVERLAY")
     local frameExpand = Art.searchResultIconFrameExpand
